f200a0d6ec11eb8cba9c8934dc3f488659706ea3
Roman Dronoff <Roman Dronoff>
2017-04-22 01:37:27 +0300

    predicates for switch were added, go is great lang!

--- main.go
+++ main.go
@@ -9,25 +9,32 @@ import (
 )
 
 const (
-	// InfoAboutDate24 is
+	// EarlyForFestival is
+	EarlyForFestival string = "В этот день фестиваль еще не начался!"
+	// ToLateForFestival is
+	ToLateForFestival string = "Сожалеем, фестиваль уже закончился"
+	//Info AboutDate24 is
 	InfoAboutDate24 string = "Показы 24 апреля: \n-Шарли и его зубы \n-Скупость \n-Парикмахер \n-Светлая сторона \n-Новая жизнь \n-Пеле"
-
 	// InfoAboutDate25 is
 	InfoAboutDate25 string = "Показы 25 апреля: \n-Фриланс \n-Лето \n-Утопия \n-Муза"
-
 	// InfoAboutUnknownDate is
 	InfoAboutUnknownDate string = "Сожалеем, в этот день фестиваль не проводится!"
-
 	// InfoAboutNotDate is
 	InfoAboutNotDate string = "Пожалуйста, введите интересующий вас день показа (23 - 25)"
 )
 
 func getInfo(date int) string {
-	switch date {
-	case 24:
+	switch {
+	case date < 24:
+		return EarlyForFestival
+
+	case date == 24:
 		return InfoAboutDate24
-	case 25:
+	case date == 25:
 		return InfoAboutDate25
+
+	case date > 29:
+		return ToLateForFestival
 	default:
 		return InfoAboutUnknownDate
 	}
